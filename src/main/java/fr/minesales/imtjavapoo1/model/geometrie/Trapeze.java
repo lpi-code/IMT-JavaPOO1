a0720b1d35275a773ec8a705573faa40660fd422
Loïc Piernas <lpiernas.pers@gmail.com>
2023-01-17 16:51:16 +0100

    Add unit test

--- src/main/java/fr/minesales/imtjavapoo1/model/geometrie/Trapeze.java
+++ src/main/java/fr/minesales/imtjavapoo1/model/geometrie/Trapeze.java
@@ -7,6 +7,9 @@ public class Trapeze extends Quadrilatere {
         super();
     }
 
+    public Trapeze(Point p1, Point p2, Point p3, Point p4) {
+        super(p1, p2, p3, p4);
+    }
 
 
     @Override
