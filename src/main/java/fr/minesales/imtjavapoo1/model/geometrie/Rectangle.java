ad1341304de7904b15f81bb5d1a5331c85f8ebe8
jaubert-nicolas <nicolas.jaubert@etu.mines-ales.fr>
2023-01-17 16:58:46 +0100

    checks added

--- src/main/java/fr/minesales/imtjavapoo1/model/geometrie/Rectangle.java
+++ src/main/java/fr/minesales/imtjavapoo1/model/geometrie/Rectangle.java
@@ -29,10 +29,17 @@ public class Rectangle extends Parallelogramme{
     @Override
     public void updateFigure(Point p1, Point p2, Point p3, Point p4){
 
-        if(Math.abs(p1.getX() - p2.getX()) != Math.abs(p3.getX() - p4.getX()))
+        double dist12 = Math.sqrt(Math.pow(p2.getX() - p1.getX(),2) + Math.pow(p2.getY() - p1.getY(),2));
+        double dist34 = Math.sqrt(Math.pow(p4.getX() - p3.getX(),2) + Math.pow(p4.getY() - p3.getY(),2));
+        double dist41 = Math.sqrt(Math.pow(p1.getX() - p4.getX(),2) + Math.pow(p1.getY() - p4.getY(),2));
+
+        double dist13 = Math.sqrt(Math.pow(p3.getX() - p1.getX(),2) + Math.pow(p3.getY() - p1.getY(),2));
+        double dist24 = Math.sqrt(Math.pow(p4.getX() - p2.getX(),2) + Math.pow(p4.getY() - p2.getY(),2));
+
+        if(Math.sqrt(Math.pow(dist12, 2) + Math.pow(dist41, 2)) != dist13)
             throw new IllegalArgumentException("Ces points ne permettent pas de faire un rectangle");
 
-        if(Math.abs(p1.getY() - p2.getY()) != Math.abs(p3.getY() - p4.getY()))
+        if(Math.sqrt(Math.pow(dist41, 2) + Math.pow(dist34, 2)) != dist24)
             throw new IllegalArgumentException("Ces points ne permettent pas de faire un rectangle");
 
         super.updateFigure(p1, p2, p3, p4);
