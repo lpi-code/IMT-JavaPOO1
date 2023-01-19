dd50a8a99c19b809e8dae68c5f94e1940e2c57bd
Loïc Piernas <lpiernas.pers@gmail.com>
2023-01-19 09:16:05 +0100

    3 points constructor

--- src/main/java/fr/minesales/imtjavapoo1/model/geometrie/Parallelogramme.java
+++ src/main/java/fr/minesales/imtjavapoo1/model/geometrie/Parallelogramme.java
@@ -10,6 +10,9 @@ public class Parallelogramme extends Trapeze {
         return "Parallélogramme";
     }
 
+    public Parallelogramme(Point p1, Point p2, Point p3){
+        super(p1,p2,p3,new Point(p1.getX() + p3.getX() - p2.getX(), p1.getY() + p3.getY() - p2.getY()));
+    }
 
     @Override
     public void updateFigure(Point p1, Point p2, Point p3, Point p4) {
