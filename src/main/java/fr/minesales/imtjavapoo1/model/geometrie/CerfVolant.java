2189b92bdfc5b8ea1bf520a44e384ece337a537b
Loïc Piernas <lpiernas.pers@gmail.com>
2023-01-23 15:10:14 +0100

    fix constructor

--- src/main/java/fr/minesales/imtjavapoo1/model/geometrie/CerfVolant.java
+++ src/main/java/fr/minesales/imtjavapoo1/model/geometrie/CerfVolant.java
@@ -12,7 +12,8 @@ public class CerfVolant extends Quadrilatere {
     }
 
     public CerfVolant(Point p1, Point p2, Point p3, Point p4) {
-        super(p1, p2, p3, p4);
+        super();
+        this.updateFigure(p1, p2, p3, p4);
     }
     @Override
     public void propriete() {
