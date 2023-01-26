65e363a5334c13b8b47c26a0d68c20518b562e56
jaubert-nicolas <nicolas.jaubert@etu.mines-ales.fr>
2023-01-26 12:08:53 +0100

    Added the controls to add a figure

--- src/main/java/fr/minesales/imtjavapoo1/DrawGeometryApp.java
+++ src/main/java/fr/minesales/imtjavapoo1/DrawGeometryApp.java
@@ -2,6 +2,7 @@ package fr.minesales.imtjavapoo1;
 
 import fr.minesales.imtjavapoo1.view.DrawGeometryView;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
@@ -10,6 +11,8 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
@@ -62,8 +65,15 @@ public class DrawGeometryApp extends Application {
                 dialog.setTitle("Créer un " + button.getText());
                 dialog.initModality(Modality.APPLICATION_MODAL);
                 dialog.initOwner(this.primaryStage);
-                this.drawGeometryView.drawPopups(button.getText());
+                TextField coordP1 = new TextField();
+                TextField coordP2 = new TextField();
+                TextField coordP3 = new TextField();
+                TextField coordP4 = new TextField();
+                ChoiceBox color = new ChoiceBox(FXCollections.observableArrayList("Vert", "Bleu", "Rouge", "Jaune", "Noir", "Gris", "Orange", "Rose", "Violet"));
+                Button validButton = new Button("Valider");
+                this.drawGeometryView.drawPopups(button.getText(), coordP1, coordP2, coordP3, coordP4, color,validButton);
                 Scene dialogScene = new Scene(this.drawGeometryView.getPopupVbox(), 400, 400);
+                validButton.setOnAction((EventHandler) event1 -> this.handleUserinput(coordP1, coordP2, coordP3, coordP4, color, button.getText()));
                 dialog.setScene(dialogScene);
                 dialog.show();
             });
@@ -71,5 +81,9 @@ public class DrawGeometryApp extends Application {
         }
 
         this.drawGeometryView.manageButton(carre, cerfVolant, losange, parallelogramme, rectangle, trapeze, quadrilatere);
+    }
+
+    public void handleUserinput(TextField coordP1, TextField coordP2, TextField coordP3, TextField coordP4, ChoiceBox color, String figure){
+        System.out.println(figure + " de coordonnées des points : " + coordP1.getText() + ", " + coordP2.getText() + ", " + coordP3.getText() + ", " + coordP4.getText() + " et couleur : " + color.getValue());
     }
 }
--- src/main/java/fr/minesales/imtjavapoo1/view/DrawGeometryView.java
+++ src/main/java/fr/minesales/imtjavapoo1/view/DrawGeometryView.java
@@ -1,5 +1,6 @@
 package fr.minesales.imtjavapoo1.view;
 
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
@@ -34,7 +35,7 @@ public class DrawGeometryView {
 
     }
 
-    public void drawPopups(String message) {
+    public void drawPopups(String message, TextField coordP1, TextField coordP2, TextField coordP3, TextField coordP4, ChoiceBox color,Button validButton) {
         this.popupVbox = new VBox(15);
         this.popupVbox.setAlignment(Pos.CENTER);
         this.popupVbox.getChildren().add(new Text("Nouveau " +  message));
@@ -42,12 +43,11 @@ public class DrawGeometryView {
         Label label2 = new Label("Coordonnées P2");
         Label label3 = new Label("Coordonnées P3");
         Label label4 = new Label("Coordonnées P4");
-        TextField coordP1 = new TextField ();
-        TextField coordP2 = new TextField ();
-        TextField coordP3 = new TextField ();
-        TextField coordP4 = new TextField ();
-        Button validButton = new Button("Valider");
-        this.popupVbox.getChildren().addAll(label1, coordP1, label2, coordP2, label3, coordP3, label4, coordP4, validButton);
+        coordP1.setMaxWidth(100);
+        coordP2.setMaxWidth(100);
+        coordP3.setMaxWidth(100);
+        coordP4.setMaxWidth(100);
+        this.popupVbox.getChildren().addAll(label1, coordP1, label2, coordP2, label3, coordP3, label4, coordP4, color,validButton);
 
     }
 
