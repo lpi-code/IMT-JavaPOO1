ef1f9e1caadbe89b70c734c61145d20883d0fcce
jaubert-nicolas <nicolas.jaubert@etu.mines-ales.fr>
2023-01-26 11:35:01 +0100

    added popup

--- src/main/java/fr/minesales/imtjavapoo1/DrawGeometryApp.java
+++ src/main/java/fr/minesales/imtjavapoo1/DrawGeometryApp.java
@@ -3,25 +3,34 @@ package fr.minesales.imtjavapoo1;
 import fr.minesales.imtjavapoo1.view.DrawGeometryView;
 import javafx.application.Application;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 public class DrawGeometryApp extends Application {
     private DrawGeometryView drawGeometryView;
+    private Stage primaryStage;
     @Override
     public void start(Stage stage) throws IOException {
 
 
         Group root = new Group();
         Scene scene = new Scene(root, 1000, 800, Color.WHITE);
-
+        this.primaryStage = stage;
         this.drawGeometryView = new DrawGeometryView();
         this.manageButton();
 
@@ -44,6 +53,23 @@ public class DrawGeometryApp extends Application {
         Button rectangle = new Button("Rectangle");
         Button trapeze = new Button("Trapeze");
         Button quadrilatere = new Button("Quadrilatere");
+
+        ArrayList<Button> buttons = new ArrayList<>(Arrays.asList(carre, cerfVolant, losange, parallelogramme, rectangle, trapeze, quadrilatere));
+
+        for ( Button button : buttons ) {
+            button.setOnAction((EventHandler) event -> {
+                final Stage dialog = new Stage();
+                dialog.setTitle("Créer un " + button.getText());
+                dialog.initModality(Modality.APPLICATION_MODAL);
+                dialog.initOwner(this.primaryStage);
+                this.drawGeometryView.drawPopups(button.getText());
+                Scene dialogScene = new Scene(this.drawGeometryView.getPopupVbox(), 400, 400);
+                dialog.setScene(dialogScene);
+                dialog.show();
+            });
+
+        }
+
         this.drawGeometryView.manageButton(carre, cerfVolant, losange, parallelogramme, rectangle, trapeze, quadrilatere);
     }
 }
--- src/main/java/fr/minesales/imtjavapoo1/view/DrawGeometryView.java
+++ src/main/java/fr/minesales/imtjavapoo1/view/DrawGeometryView.java
@@ -1,19 +1,20 @@
 package fr.minesales.imtjavapoo1.view;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.ToolBar;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 public class DrawGeometryView {
     private Canvas canvas;
     private GraphicsContext gc;
     private BorderPane borderPane;
     private ToolBar toolBar;
+    private VBox popupVbox;
     public DrawGeometryView() {
         this.borderPane = new BorderPane();
 
@@ -33,6 +34,23 @@ public class DrawGeometryView {
 
     }
 
+    public void drawPopups(String message) {
+        this.popupVbox = new VBox(15);
+        this.popupVbox.setAlignment(Pos.CENTER);
+        this.popupVbox.getChildren().add(new Text("Nouveau " +  message));
+        Label label1 = new Label("Coordonnées P1");
+        Label label2 = new Label("Coordonnées P2");
+        Label label3 = new Label("Coordonnées P3");
+        Label label4 = new Label("Coordonnées P4");
+        TextField coordP1 = new TextField ();
+        TextField coordP2 = new TextField ();
+        TextField coordP3 = new TextField ();
+        TextField coordP4 = new TextField ();
+        Button validButton = new Button("Valider");
+        this.popupVbox.getChildren().addAll(label1, coordP1, label2, coordP2, label3, coordP3, label4, coordP4, validButton);
+
+    }
+
     public void manageButton(Button carre, Button cerfVolant, Button losange, Button parallelogramme, Button rectangle, Button trapeze, Button quadrilatere) {
         this.toolBar = new ToolBar(carre, cerfVolant, losange, parallelogramme, rectangle, trapeze, quadrilatere);
         this.drawGeometryToolBar();
@@ -41,6 +59,10 @@ public class DrawGeometryView {
 
     public BorderPane getBorderPane() {
         return this.borderPane;
+    }
+
+    public VBox getPopupVbox() {
+        return this.popupVbox;
     }
 
     public ToolBar getToolBar() {
